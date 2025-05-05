b1d2c868d45c62816190c8e96bd8593eca16135c
kosonkh7 <khukwangha13@khu.ac.kr>
2025-05-05 14:17:10 +0900

    refactor: Refactor app.database.init_db.py

--- app/database/init_db.py
+++ app/database/init_db.py
@@ -2,14 +2,16 @@
 
 from sqlalchemy.orm import declarative_base
 from app.database.session import engine
-from app.models import Warehouse  # models/__init__.py 덕분에 여기서 한 번에 인식됨
+from app.models.warehouse import Base as WarehouseBase  # models/__init__.py 덕분에 여기서 한 번에 인식됨
+from app.models.inventory import Base as InventoryBase
 
 # Base를 직접 임포트하지 않고도, 모델이 모두 등록되어 있으면 create_all 가능
 from app.models.warehouse import Base
 
 def init_db():
     print("📦 Creating all tables in the database...")
-    Base.metadata.create_all(bind=engine)
+    WarehouseBase.metadata.create_all(bind=engine)
+    InventoryBase.metadata.create_all(bind=engine)
     print("✅ All tables created successfully!")
 
 # 직접 실행할 수 있게 설정
